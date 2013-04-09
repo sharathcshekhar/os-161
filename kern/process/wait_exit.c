e702b1ca29b10e9a25c13392dc4e43bea7fc3f85
Sharath <sharathcshekhar@gmail.com>
2013-04-09 04:13:48 -0400

    Revert "reserving PID 0"
    This reverts commit 37bed2ad64e95e4859d0cd58188727abb8836c33.

--- kern/process/wait_exit.c
+++ kern/process/wait_exit.c
@@ -19,13 +19,7 @@ sys_waitpid(pid_t *pid, userptr_t u_status, int options)
 	struct process_struct *child_ps_table = NULL;
 	int k_status;
 	int ret;
-	
-	if ((*pid < 2) || (*pid >= MAX_PID)) {
-		return EINVAL;
-	}
-	if (!is_pid_in_use(*pid)) {
-		return ESRCH;
-	}
+	//TODO: check if pid is valid or not	
 	if (options != 0) {
 		/* Not supported, report error */
 		return EINVAL;
